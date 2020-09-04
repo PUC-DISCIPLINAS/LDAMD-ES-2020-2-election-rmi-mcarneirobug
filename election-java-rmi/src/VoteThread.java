40219ed5de175d4afd6b796d702835a55d7ddf6c
Matheus Santos <mcarneiro@sga.pucminas.br>
2020-09-04 17:41:10 -0300

    feat: Add vote thread

---
+++ election-java-rmi/src/VoteThread.java
@@ -0,0 +1,46 @@
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+import javax.swing.JOptionPane;
+
+public class VoteThread extends Thread {
+	
+	private String candidateName;
+	private String hashVoter;
+	
+	public VoteThread(String candidateName, String hashVoter) {
+		this.candidateName = candidateName;
+		this.hashVoter = hashVoter;
+	}
+	
+	@Override
+	public void run() {
+		while(true) {
+			try {
+				Registry registry = LocateRegistry.getRegistry("localhost");
+				Election election = (Election) registry.lookup("Election");
+				
+				boolean isSuccess = election.vote(candidateName, hashVoter);
+				if(isSuccess) {
+					JOptionPane.showConfirmDialog(null, "Registered vote for candidate: " + candidateName + "!", "Sucesso",
+							JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE);
+				} else {
+					JOptionPane.showConfirmDialog(null, "This cliend already has a registered vote..", "Erro",
+							JOptionPane.DEFAULT_OPTION, JOptionPane.ERROR_MESSAGE);
+				}
+				break;
+			} catch(RemoteException | NotBoundException e) {
+				try {
+//					sleep(30000);
+					sleep(5000);
+				} catch(InterruptedException in) {
+					System.out.println("Error: " + in.getMessage());
+					in.printStackTrace();
+				}
+			} 
+		}
+	}
+
+}
